ad7f1d7a48aabb725594cf50bf35ee87202daac3
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-11-11 16:48:52 +0100

    Kinda work

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
-ss: ss.c
-	c99 ss.c -o ss
+ss: ss.c makefile
+	gcc ss.c -o ss -lffi -ggdb -lm
 
 run: ss
 	./ss
--- ss.c
+++ ss.c
@@ -1,6 +1,9 @@
+#include <ctype.h>
+#include <dlfcn.h>
 #include <ffi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* TYPES */
 // ffi_type_void;
@@ -17,12 +20,15 @@
 // ffi_type_pointer;
 // ffi_type_longdouble;
 
+#define MAX_ARGS 16
+
 typedef struct {
         char *func_name;
         ffi_cif cif;
-        ffi_type **arg_types;
-        void **arg_values;
-        size_t arg_size;
+        ffi_type *arg_types[MAX_ARGS];
+        void *arg_values[MAX_ARGS];
+        size_t arg_value_size;
+        size_t arg_type_size;
         ffi_status status;
         ffi_type *return_type;
         ffi_arg result;
@@ -31,31 +37,226 @@ typedef struct {
 void
 call_free(Call call)
 {
-        if (call.arg_types) free(call.arg_types);
-        call.arg_size = 0;
-        if (call.arg_values) free(call.arg_values);
-        call.arg_size = 0;
+        // if (call.arg_types) free(call.arg_types);
+        call.arg_value_size = 0;
+        // if (call.arg_values) free(call.arg_values);
+        call.arg_value_size = 0;
         if (call.func_name) free(call.func_name);
 }
 
 void
-call_add_arg_type(Call call, ffi_type *type)
+call_add_arg_type(Call *call, ffi_type *type)
 {
-        call.arg_types[call.arg_size] = type;
-        ++call.arg_size;
+        call->arg_types[call->arg_type_size] = type;
+        ++call->arg_type_size;
 }
 
 void
-call_add_arg_value(Call call, void *type)
+call_add_arg_value(Call *call, void *value)
 {
-        call.arg_values[call.arg_size] = type;
-        ++call.arg_size;
+        call->arg_values[call->arg_value_size] = value;
+        ++call->arg_value_size;
+}
+
+void
+ltrim(char **c)
+{
+        while (isspace(**c))
+                ++*c;
+}
+
+char *
+getid(char **c)
+{
+        char *start = *c;
+        if (!isalpha(**c) && **c != '_') return NULL;
+        ++*c;
+        while (isalnum(**c) || **c == '_') {
+                ++*c;
+        }
+        char tmp = **c;
+        **c = 0;
+        start = strdup(start);
+        **c = tmp;
+        return start;
+}
+
+int
+consume(char **s, char c)
+{
+        if (**s == c) {
+                ++*s;
+                return 1;
+        }
+        return 0;
+}
+
+int
+getint(void **value, char **s)
+{
+        char *start = *s;
+        int n = 0;
+        int m = 1;
+
+        if (consume(s, '-')) m = -1;
+        if (consume(s, '+')) m = 1;
+
+        if (!isdigit(**s)) {
+                *s = start;
+                return 1;
+        }
+
+        while (isdigit(**s)) {
+                n *= 10;
+                n += **s - '0';
+                ++*s;
+        }
+
+        if (isalpha(**s) || **s == '.') {
+                *s = start;
+                return 1;
+        }
+
+        n *= m;
+        *value = memcpy(malloc(sizeof n), &n, sizeof n);
+        return 0;
+}
+
+int
+getfloat(void **value, char **s)
+{
+        char *start = *s;
+        float n = 0;
+        float d = 1;
+        float m = 1;
+
+        if (consume(s, '-')) m = -1;
+        if (consume(s, '+')) m = 1;
+
+        if (!isdigit(**s)) {
+                *s = start;
+                return 1;
+        }
+
+        while (isdigit(**s)) {
+                n *= 10;
+                n += **s - '0';
+                ++*s;
+        }
+
+        if (!consume(s, '.')) {
+                *s = start;
+                return 1;
+        }
+
+        while (isdigit(**s)) {
+                d *= 10;
+                n += (**s - '0') / d;
+                ++*s;
+        }
+
+        *value = memcpy(malloc(sizeof n), &n, sizeof n);
+        return 0;
+}
+
+int
+getstring(void **value, char **s)
+{
+        char *start = *s;
+        if (!consume(s, '"')) {
+                *s = start;
+                return 1;
+        }
+
+        while (!consume(s, '"')) {
+                if (!**s) {
+                        *s = start;
+                        return 1;
+                }
+                ++*s;
+        }
+
+        (*s)[-1] = 0;
+        *value = strdup(start + 1);
+        *value = memcpy(malloc(sizeof *value), &*value, sizeof *value);
+        (*s)[-1] = '"';
+        return 0;
+}
+
+int
+getarg(Call *call, char **line)
+{
+        void *value;
+        if (0)
+                ;
+        else if (!getint(&value, line)) {
+                call_add_arg_value(call, value);
+                call_add_arg_type(call, &ffi_type_sint);
+                printf("INT ARG: %d\n", *(int *) value);
+        }
+
+        else if (!getfloat(&value, line)) {
+                call_add_arg_value(call, value);
+                call_add_arg_type(call, &ffi_type_float);
+                printf("FLOAT ARG: %f\n", *(float *) value);
+        }
+
+        else if (!getstring(&value, line)) {
+                call_add_arg_value(call, value);
+                call_add_arg_type(call, &ffi_type_float);
+                printf("STR ARG: %s\n", *(char **) value);
+        }
+
+        else {
+                printf("Can not parse arg\n");
+                return 1;
+        }
+        return 0;
+}
+
+int
+parse_function_call(Call *call, char *line)
+{
+        /* Parse the body as: _ ID _ "(" _ param? [_ "," _ param]* ")" _ ";"
+         * where _ is zero or more spaces and param a value that can be parsed
+         * using getarg(). ID is the name of the function. */
+        ltrim(&line);
+        call->func_name = getid(&line);
+        ltrim(&line);
+        if (!consume(&line, '(')) {
+                printf("Invalid function call");
+                return 1;
+        }
+        ltrim(&line);
+        while (!consume(&line, ')')) {
+                if (getarg(call, &line)) {
+                        printf("Can not get arg\n");
+                        return 1;
+                }
+                ltrim(&line);
+                if (!consume(&line, ',')) {
+                        if (consume(&line, ')')) {
+                                break;
+                        }
+                        printf("Can not get ')'. Invalid function args at %s\n", line);
+                        return 1;
+                }
+                ltrim(&line);
+        }
+        ltrim(&line);
+
+        /* The 0 is to return success if no semicolon was found */
+        return !consume(&line, ';') && 0;
 }
 
 int
 parse_and_run(char *line)
 {
         Call call = { 0 };
+
+        /* No yet handled */
+        call.return_type = &ffi_type_sint;
+
         if (parse_function_call(&call, line)) {
                 printf("Error: parse_function_arg_values: "
                        "Invalid line: %s\n",
@@ -66,7 +267,7 @@ parse_and_run(char *line)
         // Prepare the ffi_cif structure.
         if (ffi_prep_cif(&call.cif,
                          FFI_DEFAULT_ABI,
-                         call.arg_size,
+                         call.arg_value_size,
                          call.return_type,
                          call.arg_types) != FFI_OK) {
                 perror("Error: ffi_prep_cif");
@@ -74,16 +275,44 @@ parse_and_run(char *line)
         }
 
         // Invoke the function.
-        ffi_call(&cif, FFI_FN(foo), &result, arg_values);
+        void *func = dlsym(RTLD_DEFAULT, call.func_name);
+        if (!func) {
+                printf("Could not find function name `%s`\n", call.func_name);
+                return 1;
+        }
 
-        // The ffi_arg 'result' now contains the unsigned char returned from foo(),
-        // which can be accessed by a typecast.
+        ffi_call(&call.cif, func, &call.result, call.arg_values);
+        printf("-> %d\n", (int) call.result);
+
+        return 0;
+}
+
+void
+prompt()
+{
+        printf(">> ");
+        fflush(stdout);
 }
 
 int
 main(int argc, const char **argv)
 {
-        parse_and_run();
+        char line[1024];
+        printf("Wellcome to hsll\n");
+        printf("  You can execute C functions\n");
+        printf("  The accepted argument types are:\n");
+        printf("  - const int\n");
+        printf("  - const float\n");
+        printf("  - const string\n");
+        printf("  The accepted return type is:\n");
+        printf("  - const int\n");
+        printf("\n");
+
+        while (1) {
+                prompt();
+                if (!fgets(line, sizeof line, stdin)) break;
+                parse_and_run(line);
+        }
 
         return 0;
 }
