61f289e8937e07bdb37d9cd5b256f7cfe1220597
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-11-11 11:46:46 +0100

    working on it)

--- ss.c
+++ ss.c
@@ -1,165 +1,89 @@
-#define _POSIX_C_SOURCE 200809L // dprintf
-
-#include <errno.h>
-#include <stdbool.h>
+#include <ffi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <unistd.h>
 
-enum RUNOPT {
-        SYNC = 1,
-        ASYNC = 0,
-};
+/* TYPES */
+// ffi_type_void;
+// ffi_type_uint8;
+// ffi_type_sint8;
+// ffi_type_uint16;
+// ffi_type_sint16;
+// ffi_type_uint32;
+// ffi_type_sint32;
+// ffi_type_uint64;
+// ffi_type_sint64;
+// ffi_type_float;
+// ffi_type_double;
+// ffi_type_pointer;
+// ffi_type_longdouble;
 
-#ifndef strdup
-#define strdup(s) memcpy(malloc(strlen((s)) + 1), (s), strlen((s)) + 1)
-#endif
-
-char *expand(char *);
-void prompt();
-
-static char *PROMPT = ">> ";
-static bool alive = true;
+typedef struct {
+        char *func_name;
+        ffi_cif cif;
+        ffi_type **arg_types;
+        void **arg_values;
+        size_t arg_size;
+        ffi_status status;
+        ffi_type *return_type;
+        ffi_arg result;
+} Call;
 
 void
-prompt()
+call_free(Call call)
 {
-        printf("%s", expand(PROMPT));
-}
-
-char *
-expand(char *str)
-{
-        return str;
-}
-
-/* ret and *ret must be free */
-char **
-split(char *str, char sep)
-{
-        char **arr = malloc(sizeof(char *));
-        int arrlen = 1;
-        char *c;
-
-        arr[0] = c = strdup(str);
-        for (; *c; c++) {
-                if (*c != sep) continue;
-                *c = 0;
-                arr = realloc(arr, sizeof(char *) * (arrlen + 1));
-                arr[arrlen] = c + 1;
-                ++arrlen;
-        }
-        return arr;
+        if (call.arg_types) free(call.arg_types);
+        call.arg_size = 0;
+        if (call.arg_values) free(call.arg_values);
+        call.arg_size = 0;
+        if (call.func_name) free(call.func_name);
 }
 
 void
-freesplit(char **split)
+call_add_arg_type(Call call, ffi_type *type)
 {
-        free(*split);
-        free(split);
+        call.arg_types[call.arg_size] = type;
+        ++call.arg_size;
+}
+
+void
+call_add_arg_value(Call call, void *type)
+{
+        call.arg_values[call.arg_size] = type;
+        ++call.arg_size;
 }
 
 int
-run(char **argv, int fdin, int fdout, bool sync, int *pid)
+parse_and_run(char *line)
 {
-        int child;
-        int status = 0;
-
-        switch (child = fork()) {
-        case -1:
-                perror("fork");
-                return -1;
-
-        case 0:
-                dup2(fdin, STDIN_FILENO);
-                dup2(fdout, STDOUT_FILENO);
-
-                status = execvp(argv[0], argv);
-                dprintf(fdout, "%s: %s\n", argv[0], strerror(errno));
-                exit(status);
-
-        default:
-                if (pid) *pid = child;
-                if (sync) waitpid(child, &status, 0);
-                return status;
-        }
-}
-
-int
-cd(char **argv)
-{
-        return chdir(argv[1]);
-}
-
-bool
-is_builtin(char *cmd)
-{
-        return !strcmp("cd", cmd) ||
-               !strcmp("exit", cmd) ||
-               0; // style reasons
-}
-
-int
-run_builtin(char **argv, int fdin, int fdout, bool sync, int *pid)
-{
-        int child;
-        int status = 0;
-        int fdinold, fdoutold;
-
-        if (sync == ASYNC) {
-                fprintf(stderr, "builtin cmd (%s) should be sync\n", *argv);
+        Call call = { 0 };
+        if (parse_function_call(&call, line)) {
+                printf("Error: parse_function_arg_values: "
+                       "Invalid line: %s\n",
+                       line);
                 return 1;
         }
 
-        fdinold = dup(STDIN_FILENO);
-        fdoutold = dup(STDOUT_FILENO);
-        dup2(fdin, STDIN_FILENO);
-        dup2(fdout, STDOUT_FILENO);
+        // Prepare the ffi_cif structure.
+        if (ffi_prep_cif(&call.cif,
+                         FFI_DEFAULT_ABI,
+                         call.arg_size,
+                         call.return_type,
+                         call.arg_types) != FFI_OK) {
+                perror("Error: ffi_prep_cif");
+                return 1;
+        }
 
-        // clang-format off
-        if (0); // style reasons 
-        else if (!strcmp("cd", *argv)) status = cd(argv);
-        else if (!strcmp("exit", *argv)) alive = false;
-        // clang-format on
+        // Invoke the function.
+        ffi_call(&cif, FFI_FN(foo), &result, arg_values);
 
-        dup2(STDIN_FILENO, fdinold);
-        dup2(STDOUT_FILENO, fdoutold);
-
-        return status;
+        // The ffi_arg 'result' now contains the unsigned char returned from foo(),
+        // which can be accessed by a typecast.
 }
 
 int
-execute(char *cmd)
+main(int argc, const char **argv)
 {
-        char **scmd = split(cmd, ' ');
+        parse_and_run();
 
-        (is_builtin(scmd[0]) ? run_builtin : run)
-        /* Call the prev function with this arguments */
-        (scmd, STDIN_FILENO, STDOUT_FILENO, SYNC, NULL);
-
-        freesplit(scmd);
         return 0;
-}
-
-char *
-getinput()
-{
-/*   */ #define SIZE 1024
-        char *buf = malloc(SIZE);
-        while (!fgets(buf, SIZE, stdin)) {
-                prompt();
-        }
-        buf[SIZE - 1] = buf[strcspn(buf, "\n\r")] = 0;
-        return buf;
-}
-
-int
-main()
-{
-        while (alive) {
-                prompt();
-                execute(getinput());
-        }
 }
